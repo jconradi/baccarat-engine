8715df8b30914af9f0cc2b8d00db6e37b6efe070
Jeff Conradi <jeffreyconradi@gmail.com>
2020-03-12 15:28:50 -0700

    Add BaccaratGameEngine to main module export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const BaccaratResultsEngine = require('./baccaratResultsEngine.js');
+const BaccaratGameEngine = require('./gameEngine/baccaratGameEngine.js');
 const Card = require('./card.js');
 const GameResult = require('./gameResult.js');
 const Hand = require('./hand.js');
@@ -9,6 +10,7 @@ const RoadmapGenerator = require('./roadmaps/roadmapGenerator.js');
 
 module.exports = {
     BaccaratResultsEngine: BaccaratResultsEngine,
+    BaccaratGameEngine: BaccaratGameEngine,
     Card: Card,
     GameResult: GameResult,
     Hand: Hand,
